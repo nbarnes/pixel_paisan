f8f25aaaa11bce1c3474528cbdc5dd06a49d10a2
NBarnes <nbarnes@gmail.com>
2015-01-24 22:47:16 -0800

    Fix to config.action_controller.asset_host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "//s3.amazonaws.com/#{ENV['S3_BUCKET_NAME']}"
+  config.action_controller.asset_host = "//#{ENV['S3_BUCKET_NAME']}.s3.amazonaws.com"
+
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
