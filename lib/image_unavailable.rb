775da8e2ca1ce1701e40591245cc259ada834a72
nbarnes <nbarnes@gmail.com>
2016-01-18 03:46:07 -0800

    Whitespace fix

--- lib/image_unavailable.rb
+++ lib/image_unavailable.rb
@@ -1,5 +1,5 @@
 
-  module ImageUnavailable
+module ImageUnavailable
 
   def image_unavailable(size_tag)
     return File.open("app/assets/images/image_unavailable_#{size_tag}.png") { |io| image = ChunkyPNG::Image.from_io(io) }
