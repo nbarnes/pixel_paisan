28013de05be6d34f6482fffd282a44779468737c
nbarnes <nbarnes@gmail.com>
2016-10-22 16:07:40 -0700

    Removed some stray debugging statements from tests

--- test/integration/pictures_test.rb
+++ test/integration/pictures_test.rb
@@ -42,10 +42,7 @@ feature 'Pictures' do
   # http://stackoverflow.com/questions/40098319/rails-javascript-test-fails-on-travis-ci-works-locally
 
   scenario 'as a picture owner, I can change the name of a picture', js: true do
-    puts 'outside'
-    puts ENV['ON_TRAVIS']
     if ENV['ON_TRAVIS'] != 'true'
-      puts 'inside'
       login_user
       visit picture_path(pictures(:tony_picture01).id)
       page.must_have_content 'tony_picture01'
