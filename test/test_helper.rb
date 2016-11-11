d3529eb8049f8bddb4d4d7a27bf0a33ff5e51b9d
nbarnes <nbarnes@gmail.com>
2016-11-10 16:08:52 -0800

    Increased poltergeist timeout in an attempt to help Travis find its ass with both hands

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,9 +12,9 @@ class ActiveSupport::TestCase
 
   # Capybara.javascript_driver = :webkit
 
-  # Capybara.register_driver :poltergeist do |app|
-  #   Capybara::Poltergeist::Driver.new(app, timeout: 180)
-  # end
+  Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new(app, timeout: 180)
+  end
 
   Capybara.javascript_driver = :poltergeist
 
